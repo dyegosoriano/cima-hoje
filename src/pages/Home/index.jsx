7b47485ca4377ce6766dea96be58947f84f95a3a
Dyego Soriano <dyegodf2@gmail.com>
2021-01-21 20:26:45 -0300

    buscando dados da api openweathermap

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,20 +4,45 @@ import { Button, TextField } from '@material-ui/core'
 import { FormStyled } from './styles'
 
 export default function Home() {
+  const [erro, setErro] = useState(false)
   const [city, setCity] = useState('')
 
   async function handleSubmit() {
-    console.log(city)
+    if (!city) return
+
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city},br&appid=${process.env.REACT_APP_KEY_OPEN_WEATHER}`
+      )
+        .then(response => response.json())
+        .then(response => response)
+
+      if (response.cod === '404') {
+        setErro(true)
+        return
+      }
+
+      console.log(response)
+    } catch (error) {
+      console.log(`error.message >>> ${error.message} <<<`)
+    }
   }
 
   return (
     <FormStyled>
       <TextField
-        onChange={event => setCity(event.target.value)}
+        onChange={event => {
+          setCity(event.target.value)
+          setErro(false)
+        }}
         label="Informe a cidade"
         className="text__input"
         value={city}
       />
+
+      {erro && (
+        <span className="msg__error">Você inseriu uma cidade inexistente!</span>
+      )}
 
       <Button
         className="handle__button"
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,18 +8,24 @@ export const FormStyled = styled.div`
   flex-direction: column;
   align-items: center;
 
+  .text__input {
+    padding-bottom: 16px;
+    width: 80%;
+  }
+
+  .msg__error {
+    font-weight: 300;
+    font-size: 1rem;
+    color: #ff1744;
+  }
+
   .handle__button {
     background: #0091ea;
+    margin-top: 16px;
     color: #fff;
-    margin-top: 32px;
 
     &:hover {
       background: #01579b;
     }
-  }
-
-  .text__input {
-    padding-bottom: 20px;
-    width: 80%;
   }
 `
