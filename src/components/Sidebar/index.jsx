38a98428fb5e280a4025e34ff4cdeb108c3ae3e8
Dyego Soriano <dyegodf2@gmail.com>
2021-01-21 17:29:13 -0300

    criando componente Sidebar

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Aside } from './styles'
+
+export default function Sidebar() {
+  return (
+    <Aside>
+      <h1>Clima Hoje</h1>
+
+      <p>
+        Descubra a temperatura e as condições climáticas atuais de qualquer
+        cidade.
+      </p>
+    </Aside>
+  )
+}
---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+import bg from '../../assets/bg.png'
+
+export const Aside = styled.aside`
+  background: url(${bg}) no-repeat;
+  background-size: cover;
+
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    font-weight: 300;
+    text-align: center;
+    padding: 16px;
+  }
+`
