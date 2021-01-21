530475f370751a368f9159f3993571eaef79c5b0
Dyego Soriano <dyegodf2@gmail.com>
2021-01-21 13:25:46 -0300

    criando componente BoxContainer

---
+++ src/components/BoxContainer/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Box } from '@material-ui/core'
+
+export const Container = styled(Box)`
+  max-width: 900px;
+  max-height: 600px;
+
+  background-color: #fff;
+`
