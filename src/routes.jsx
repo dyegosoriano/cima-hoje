90d7e8584c646c90f20e8bcf793e9eae1b6fc0db
Dyego Soriano <dyegodf2@gmail.com>
2021-01-22 13:52:01 -0300

    refatorando rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import CityWeather from './pages/CityWeather'
 import Home from './pages/Home'
 
 export default function Routes() {
@@ -8,6 +9,7 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/city" component={CityWeather} />
       </Switch>
     </BrowserRouter>
   )
